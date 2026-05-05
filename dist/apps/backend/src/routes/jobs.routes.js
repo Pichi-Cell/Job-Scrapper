17bc78edc894e8501ac6908b4eefee762c913b55
Pichi-Cell <Picchilucas7@gmail.com>
2026-05-05 16:56:55 -0300

    gitignore

--- dist/apps/backend/src/routes/jobs.routes.js
+++
@@ -1,4 +0,0 @@
-import { Router } from "express";
-import { handleJobsRequest } from "../api/jobs.controller.js";
-export const jobsRouter = Router();
-jobsRouter.get("/", handleJobsRequest);
--- dist/apps/backend/src/scrapers/index.js
+++
@@ -1,4 +0,0 @@
-export { AccentureScraper } from "./accenture.scraper.js";
-export { EyScraper } from "./ey.scraper.js";
-export { GoogleScraper } from "./google.scraper.js";
-export { IbmTalentScraper } from "./ibm-talent.scraper.js";
